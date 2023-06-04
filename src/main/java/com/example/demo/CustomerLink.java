8323fea77df79e2a2e0764cb562c8c9edbf21a3b
John Chase <ratteal@gmail.com>
2023-06-04 19:51:57 +0100

    Remove unused imports.

--- src/main/java/com/example/demo/CustomerLink.java
+++ src/main/java/com/example/demo/CustomerLink.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
